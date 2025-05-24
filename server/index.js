16ebb59a492fd0d2f7477f16d58818ef11288c54
LAPTOP-7I4N09T2\Bella <dhp951320@gmail.com>
2025-05-24 11:17:19 +0800

    feat : add app.use ( route ) for index.js

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const eventRoutes = require('./routes/eventRoute');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use('/event', eventRoutes);
+
+app.listen(3000, () => {
+  console.log('伺服器已啟動 http://localhost:3000');
+});
+
