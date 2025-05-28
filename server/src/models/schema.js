ec4d6d70d6d07e64b7ea865194c36c16825edc9c
jasminechen95 <tingone59@gmail.com>
2025-05-29 01:41:22 +0800

    feat: add user and related tables (notifications, collections, folders)

--- server/src/models/schema.js
+++ server/src/models/schema.js
@@ -1,4 +1,90 @@
-const { pgTable, varchar, bigint, timestamp, integer, index, smallint, serial, primaryKey  } = require('drizzle-orm/pg-core');
+const { pgTable, varchar, bigint, timestamp, integer, index, smallint, serial, primaryKey,
+  pgEnum, date, boolean, text, numeric } = require('drizzle-orm/pg-core');
+
+const providerTypeEnum = pgEnum("provider_type", ["email", "google", "line"]);
+const roleEnum = pgEnum("role", ["admin", "user"]);
+const NotificationTypeEnum = pgEnum("notification_type", ["new_participant", "new_comment"]);
+
+const usersTable = pgTable("users", {
+  id: serial().primaryKey(),
+  username: varchar({ length: 100 }).notNull().unique(),
+  nickname: varchar({ length: 100 }),
+  email: varchar({ length: 100 }).notNull().unique(),
+  password: varchar({ length: 100 }).notNull(),
+  role: roleEnum().default('user'),
+  birthday: date(),
+  isVerifiedEmail: boolean("is_verified_email").default(false),
+  providerType: providerTypeEnum("provider_type"),
+  providerId: varchar("provider_id", { length: 100 }),
+  avatarUrl: varchar("avatar_url", { length: 255 }),
+  avatarKey: varchar("avatar_key", { length: 255 }),
+  avatarLastUpdated: timestamp("avatar_last_updated").defaultNow(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+  status: smallint("status").default(1).notNull(),
+});
+
+const userNotificationTable = pgTable("user_notification", {
+  id: serial().primaryKey(),
+  userId: integer("user_id").notNull().references(() => usersTable.id, { onDelete: 'cascade' }),
+  notificationType: NotificationTypeEnum("notification_type").notNull(),
+  content: text("content").notNull(),
+  isRead: boolean("is_read").default(false),
+  createdAt: timestamp("created_at").defaultNow(),
+});
+
+const userBarCollectionTable = pgTable("user_bar_collection", {
+  id: serial().primaryKey(),
+  userId: integer("user_id").references(() => usersTable.id, { onDelete: "cascade" }),
+  barId: integer("bar_id").references(() => barsTable.id, { onDelete: "cascade" }),
+  folderId: integer("folder_id").references(() => userBarFolderTable.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at").defaultNow(),
+}, (table) => ({
+  userBarUnique: unique().on(table.userId, table.barId),
+}));
+
+const userBarFolderTable = pgTable("user_bar_folder", {
+  id: serial().primaryKey(),
+  userId: integer().references(() => usersTable.id, { onDelete: "cascade" }),
+  folderName: varchar("folder_name", { length: 50 }),
+  createdAt: timestamp("created_at").defaultNow(),
+})
+
+const barsTable = pgTable("bars", {
+  id: serial().primaryKey(),
+  name: varchar({ length: 100 }).notNull(),
+  address: varchar({ length: 255 }).notNull(),
+  phone: varchar({ length: 20 }).notNull(),
+  description: text(),
+  rating: numeric("rating", { precision: 3, scale: 1 }), // 計算至小數點一位
+  openHours: varchar("open_hours", { length: 50 }),
+  createdAt: timestamp("created_at").defaultNow(),
+});
+
+const userEventCollectionTable = pgTable("user_event_collection", {
+  id: serial().primaryKey(),
+  userId: integer("user_id").references(() => usersTable.id, { onDelete: "cascade" }),
+  eventId: bigint("event_id", { mode: "string" }).references(() => events.id, { onDelete: "cascade" }),
+  folderId: integer("folder_id").references(() => userEventFolderTable.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at").defaultNow(),
+}, (table) => ({
+  userEventUnique: unique().on(table.userId, table.eventId),
+}));
+
+const userEventParticipationTable = pgTable("user_event_participation", {
+  id: serial().primaryKey(),
+  userId: integer("user_id").references(() => usersTable.id, { onDelete: "cascade" }),
+  eventId: bigint("event_id", { mode: "string" }).references(() => events.id, { onDelete: "cascade" }),
+  joinedAt: timestamp("joined_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+});
+
+const userEventFolderTable = pgTable("user_event_folder", {
+  id: serial().primaryKey(),
+  userId: integer().references(() => usersTable.id, { onDelete: "cascade" }),
+  folderName: varchar("folder_name", { length: 50 }),
+  createdAt: timestamp("created_at").defaultNow(),
+})
 
 const events = pgTable('events', {
   id: bigint('id').primaryKey(),
@@ -12,18 +98,18 @@ const events = pgTable('events', {
   price: integer('price'),
   hostUser: integer('host_user').notNull().references('users', 'id', { onDelete: 'cascade' }),
   createdAt: timestamp('created_at').notNull(),
-  modifyAt: timestamp('modify_at').notNull(), 
+  modifyAt: timestamp('modify_at').notNull(),
   status: smallint('status').default(1).notNull(), //1: 正常，2: 刪除
 }, (table) => ({
   hostUserIdx: index('idx_host_user').on(table.hostUser),
 }));
 
-const tags  = pgTable('tags', {
+const tags = pgTable('tags', {
   id: serial('id').primaryKey(),
   name: varchar('name', { length: 50 }),
 });
 
-const eventTags  = pgTable('event_tags', {
+const eventTags = pgTable('event_tags', {
   eventId: bigint('event_id').notNull().references('events', 'id', { onDelete: 'cascade' }),
   tagId: integer('tag_id').notNull().references('tags', 'id', { onDelete: 'cascade' }),
 }, (table) => ({
@@ -32,4 +118,16 @@ const eventTags  = pgTable('event_tags', {
 
 
 
-module.exports = { events, tags, eventTags };
+module.exports = {
+  usersTable,
+  userNotificationTable,
+  userBarCollectionTable,
+  userBarFolderTable,
+  barsTable,
+  userEventCollectionTable,
+  userEventParticipationTable,
+  userEventFolderTable,
+  events,
+  tags,
+  eventTags
+};
